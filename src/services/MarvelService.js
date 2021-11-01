bbda1ccdb46ec4dec8895e9ef5406427e0e8bd0e
Yura <yura04091@gmail.com>
2021-11-01 10:59:08 +0200

    load

--- src/services/MarvelService.js
+++ src/services/MarvelService.js
@@ -1,8 +1,10 @@
 
 
 class MarvelService {
-    _apiBase = 'https://gateway.marvel.com:443/v1/public/'
-    _apiKey = 'apikey=97f79fa69b594ee154c959157e437a14'
+    _apiBase = 'https://gateway.marvel.com:443/v1/public/';
+    _apiKey = 'apikey=97f79fa69b594ee154c959157e437a14';
+    _baseOffset = 210;
+
     getResource = async (url) =>{   
         let res = await fetch(url);
     
@@ -13,8 +15,8 @@ class MarvelService {
         return await res.json();
     };
 
-    getAllCharacters = async() => {
-        const res = await this.getResource(`${this._apiBase}characters?limit=9&offset=210&${this._apiKey}`);
+    getAllCharacters = async(offset = this._baseOffset) => {
+        const res = await this.getResource(`${this._apiBase}characters?limit=9&offset=${offset}&${this._apiKey}`);
         return res.data.results.map(this._transformCharacter);
     }
 
